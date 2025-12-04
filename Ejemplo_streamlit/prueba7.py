56cd1c7246683d1b0ccfed623dffbc4d946c1b75
jarsmp37 <133422734+jarsmp37@users.noreply.github.com>
2025-12-04 10:14:16 -0600

    ASD

--- Ejemplo_streamlit/prueba7.py
+++ Ejemplo_streamlit/prueba7.py
@@ -1,17 +1,13 @@
 import streamlit as st
-
 st.title("Ejemplo de Layouts en Streamlit")
-
 # Sidebar
 st.sidebar.header("Configuraciones")
 nombre = st.sidebar.text_input("Nombre del usuario:")
 mostrar_info = st.sidebar.checkbox("Mostrar información adicional")
-
 # Columnas
 col1, col2 = st.columns(2)
 col1.write("Columna 1: puede contener texto, imágenes o gráficos.")
 col2.write("Columna 2: útil para comparaciones o métricas.")
-
 # Expander
 with st.expander("Ver detalles"):
     st.write("Este texto está dentro de un expander y se puede ocultar.")
